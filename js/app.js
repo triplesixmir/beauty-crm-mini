ee54e99d8accebe608e71f54eb6b2a87f72a307d
Egor Sannikov <exnessys@gmail.com>
2026-05-05 18:44:20 +0300

    добавил разметку и стили для формы добавления клиента, подключил библиотеку inputmask для поля телефона и написал js-логику для обработки отправки формы и сохранения данных клиента

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
-console.log('Happy developing ✨')
+Inputmask("+7 999 999-99-99").mask("#client-tel");
+
+// Обработка добавления нового клиента
+
+const form = document.getElementById('form');
+const clientsContainer = document.getElementById('clients-section__content');
+
+let clients = [];
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById('client-name').value;
+    const tel = document.getElementById('client-tel').value;
+    const telegram = document.getElementById('client-tg').value;
+
+    const newClient = {
+        id: clients.length + 1,
+        name,
+        tel,
+        telegram
+    };
+
+    clients.push(newClient);
+
+    renderClients();
+
+    form.reset();
+});
+
+function renderClients() {
+
+}
