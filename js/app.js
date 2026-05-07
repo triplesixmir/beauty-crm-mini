6fb14f192f494adbdc154be0b7cc6dc487969308
Egor Sannikov <exnessys@gmail.com>
2026-05-07 23:11:30 +0300

    добавил функционал редактирования клиента, сделал номер телефона и telegram кликабельными ссылками в карточке, убрал атрибуты action и method у формы

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ const form = document.getElementById('form');
 const clientsContainer = document.getElementById('clients-section__content');
 
 let clients = JSON.parse(localStorage.getItem('clients')) || [];
+let editingClientId = null;
 renderClients();
 
 function saveClients() {
@@ -14,25 +15,45 @@ function saveClients() {
 
 }
 
+// Добавление клиента
 form.addEventListener('submit', function (event) {
     event.preventDefault();
 
     const name = document.getElementById('client-name').value;
     const tel = document.getElementById('client-tel').value;
-    const telegram = document.getElementById('client-tg').value;
+    const telegram = document.getElementById('client-tg').value.replace('@', '');
 
-    const newClient = {
-        id: Date.now(),
-        name,
-        tel,
-        telegram
-    };
+    if (editingClientId) {
+        // обновить клиента через map
+        clients = clients.map(client => {
+           if(client.id === editingClientId) {
+               return {
+                   ...client,
+                   name,
+                   tel,
+                   telegram
+               }
+           } else {
+               return client;
+           }
+        });
 
-    clients.push(newClient);
+    } else {
+        // создать newClient и push
+        const newClient = {
+            id: Date.now(),
+            name,
+            tel,
+            telegram
+        };
+
+        clients.push(newClient);
+
+    }
+
+    editingClientId = null;
     saveClients();
-
     renderClients();
-
     form.reset();
 });
 
@@ -42,6 +63,8 @@ function renderClients() {
     clients.forEach(client => {
         const card = document.createElement('div');
         card.className = 'client-card';
+
+        const cleanTel = client.tel.replace(/\D/g, '');
 
         card.innerHTML = `
         <div class="client-card__main-info">
@@ -53,17 +76,19 @@ function renderClients() {
         </div>
 
         <div class="client-card__details">
-            <p><span>Телефон</span><a href="tel:${client.tel}">${client.tel}</a></p>
-            <p><span>Telegram</span>${client.telegram}</p>
+            <p><span>Телефон</span><a href="tel:+${cleanTel}">${client.tel}</a></p>
+            <p><span>Telegram</span><a href="https://t.me/${client.telegram.replace('@', '')}" target="_blank" rel="noopener noreferrer">${client.telegram}</a></p>
         </div>
         
         <button class="client-card__delete-btn" data-id="${client.id}">Удалить</button>
+        <button class="client-card__edit-btn" data-id="${client.id}">Редактировать</button>
         `;
 
         clientsContainer.appendChild(card);
         });
 }
 
+// Поведение кнопки при удалении клиента
 clientsContainer.addEventListener('click', function (event) {
     if(event.target.classList.contains('client-card__delete-btn')) {
         const clientId = Number(event.target.dataset.id);
@@ -72,5 +97,20 @@ clientsContainer.addEventListener('click', function (event) {
         saveClients();
 
         renderClients();
+    }
+
+    // Поведение кнопки при редактировании клиента
+    if(event.target.classList.contains('client-card__edit-btn')) {
+
+        const clientId = Number(event.target.dataset.id);
+        const clientToEdit = clients.find(client => client.id === clientId);
+
+        // Ищем каждый инпут и вставляем туда инфу нашего клиента
+        document.getElementById('client-name').value = clientToEdit.name;
+        document.getElementById('client-tel').value = clientToEdit.tel;
+        document.getElementById('client-tg').value = clientToEdit.telegram;
+
+        editingClientId = clientId;
+
     }
 });
