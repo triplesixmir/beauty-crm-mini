2160c6313e0f169673e3fa8bbf1f75fff64bbc02
Egor Sannikov <exnessys@gmail.com>
2026-06-15 20:28:39 +0300

    добавил маршрутизацию с помощью react-router: обернул приложение в BrowserRouter и настроил роуты в App.jsx; создал базовые компоненты-страницы для всех разделов (дэшборд, клиенты, записи, финансы, сотрудники, статистика, настройки) и перенес код главной страницы в отдельный компонент DashboardPage; обновил боковую навигацию в SideNavbar, добавив NavLink, пути и иконки lucide-react; добавил смысловые комментарии для группировки стейтов, модалок и логики сайдбара в App.jsx

--- src/components/navbars/SideNavbar.jsx
+++ src/components/navbars/SideNavbar.jsx
@@ -1,14 +1,65 @@
+import {LayoutDashboard as LayoutDashboardIcon} from "lucide-react";
+import {UsersRound as UsersRoundIcon} from "lucide-react";
+import {CalendarDays as CalendarDaysIcon} from "lucide-react";
+import {Wallet as WalletIcon} from "lucide-react";
+import {IdCardLanyard as IdCardLanyardIcon} from "lucide-react";
+import {ChartColumnBig as ChartColumnBigIcon} from "lucide-react";
+import {Settings as SettingsIcon} from "lucide-react";
+import {NavLink} from "react-router";
+
 export function SideNavbar() {
 
   const NAVBAR_ITEMS = [
     {
-      name: "Dashboard",
-      icon: <DashboardIcon />,
+      name: "Дэшборд",
+      Icon: LayoutDashboardIcon,
       path: "/dashboard"
-    }
+    },
+    {
+      name: "Клиенты",
+      Icon: UsersRoundIcon,
+      path: "/clients"
+    },
+    {
+      name: "Записи",
+      Icon: CalendarDaysIcon,
+      path: "/appointments"
+    },
+    {
+      name: "Финансы",
+      Icon: WalletIcon,
+      path: "/finances"
+    },
+    {
+      name: "Сотрудники",
+      Icon: IdCardLanyardIcon,
+      path: "/employees"
+    },
+    {
+      name: "Статистика",
+      Icon: ChartColumnBigIcon,
+      path: "/statistics"
+    },
+    {
+      name: "Настройки",
+      Icon: SettingsIcon,
+      path: "/settings"
+    },
   ]
 
+
   return (
-    <></>
+    <>
+      {NAVBAR_ITEMS.map((item) => (
+        <NavLink
+          key={item.name}
+          to={item.path}
+          className="side-navbar__item"
+        >
+          <item.Icon />
+          {item.name}
+        </NavLink>
+      ))}
+    </>
   )
 }
